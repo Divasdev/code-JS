66fc22e230c7f1e6f86552d7281cbaebeddfe120
Divasdev <sharmadivas881@gmail.com>
2025-12-09 20:08:44 +0530

    teranry-codewars

---
+++ be-concise-ternaryoperator.js
@@ -0,0 +1,9 @@
+ // TODO: Refactor and shorten the function
+
+function describeAge(age) {
+  return age<=12 ? "You're a(n) kid":
+         age<=17 ? "You're a(n) teenager":
+         age<=64 ? "You're a(n) adult":
+                   "You're a(n) elderly";
+}
+
